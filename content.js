0738896554b81e37fa51e7bef7c654d9c24c6b1a
ottoheiskanen <ottoheiskanen@gmail.com>
2024-06-03 15:39:20 +0300

    save

--- content.js
+++ content.js
@@ -1,3 +1,52 @@
+async function inlineResources(newTab, fileUrl) {
+  const baseUrl = fileUrl.substring(0, fileUrl.lastIndexOf('/'));
+
+  setBaseUrl(newTab, baseUrl);
+
+  try {
+    await Promise.all([
+      inlineStyles(newTab, baseUrl),
+      inlineImages(newTab, baseUrl)
+    ]);
+  } catch (error) {
+    console.error('Error inlining resources:', error);
+  }
+}
+
+function setBaseUrl(newTab, baseUrl) {
+  const baseElement = newTab.document.createElement('base');
+  baseElement.href = baseUrl + '/';
+  newTab.document.head.appendChild(baseElement);
+}
+
+async function inlineStyles(newTab, baseUrl) {
+  const linkTags = Array.from(newTab.document.querySelectorAll('link[rel="stylesheet"]'));
+  const fetchCssPromises = linkTags.map(tag => fetchAndInlineStyle(tag, baseUrl));
+  await Promise.all(fetchCssPromises);
+}
+
+async function fetchAndInlineStyle(tag, baseUrl) {
+  const cssUrl = new URL(tag.href, baseUrl).href;
+  try {
+    const response = await fetch(cssUrl);
+    if (!response.ok) throw new Error(`Failed to fetch CSS: ${response.statusText}`);
+    const cssContent = await response.text();
+    const styleElement = document.createElement('style');
+    styleElement.textContent = cssContent;
+    tag.parentNode.replaceChild(styleElement, tag);
+  } catch (error) {
+    console.error(`Error fetching CSS from ${cssUrl}:`, error);
+  }
+}
+
+async function inlineImages(newTab, baseUrl) {
+  const imgTags = Array.from(newTab.document.querySelectorAll('img'));
+  imgTags.forEach(tag => {
+    const imgUrl = new URL(tag.src, baseUrl).href;
+    tag.src = imgUrl;
+  });
+}
+
 function addButtons() {
   const fileElements = document.querySelectorAll('div.react-directory-filename-column h3 a[title$=".html"]');
 
@@ -16,9 +65,11 @@ function addButtons() {
           const response = await fetch(fileUrl);
           const htmlContent = await response.text();
           
+          const sanitizedHtmlContent = removeScriptTags(htmlContent);
+
           const newTab = window.open();
           newTab.document.open();
-          newTab.document.write(htmlContent);
+          newTab.document.write(sanitizedHtmlContent);
           newTab.document.close();
 
           await inlineResources(newTab, fileUrl);
@@ -32,73 +83,12 @@ function addButtons() {
   });
 }
 
-async function inlineResources(newTab, fileUrl) {
-  const baseUrl = fileUrl.substring(0, fileUrl.lastIndexOf('/'));
-
-  // update base URL for relative paths
-  const baseElement = newTab.document.createElement('base');
-  baseElement.href = baseUrl + '/';
-  newTab.document.head.appendChild(baseElement);
-
-  // Inline styles
-  const linkTags = Array.from(newTab.document.querySelectorAll('link[rel="stylesheet"]'));
-  for (const tag of linkTags) {
-    const cssUrl = new URL(tag.href, baseUrl).href;
-    try {
-      const response = await fetch(cssUrl);
-      const cssContent = await response.text();
-      const styleElement = newTab.document.createElement('style');
-      styleElement.textContent = cssContent;
-      tag.parentNode.replaceChild(styleElement, tag);
-    } catch (error) {
-      console.error('Error fetching CSS:', error);
-    }
-  }
-
-  // Inline scripts and ensure they execute in order
-  const scriptTags = Array.from(newTab.document.querySelectorAll('script'));
-  for (const tag of scriptTags) {
-    if (tag.src) {
-      const scriptUrl = new URL(tag.src, baseUrl).href;
-      try {
-        const response = await fetch(scriptUrl);
-        const scriptContent = await response.text();
-        const inlineScript = newTab.document.createElement('script');
-        inlineScript.textContent = scriptContent;
-        tag.parentNode.replaceChild(inlineScript, tag);
-        // Execute the script immediately
-        newTab.eval(inlineScript.textContent);
-      } catch (error) {
-        console.error('Error fetching script:', error);
-      }
-    } else {
-      // Execute inline scripts immediately
-      newTab.eval(tag.textContent);
-    }
-  }
-
-  // Fix image paths
-  const imgTags = Array.from(newTab.document.querySelectorAll('img'));
-  for (const tag of imgTags) {
-    const imgUrl = new URL(tag.src, baseUrl).href;
-    tag.src = imgUrl;
-  }
-
-  // re-execute inline scripts to ensure they run correctly
-  reexecuteScripts(newTab);
-}
-
-function reexecuteScripts(newTab) {
-  const scriptTags = Array.from(newTab.document.querySelectorAll('script'));
-  scriptTags.forEach(scriptTag => {
-    const newScriptTag = newTab.document.createElement('script');
-    if (scriptTag.src) {
-      newScriptTag.src = scriptTag.src;
-    } else {
-      newScriptTag.textContent = scriptTag.textContent;
-    }
-    scriptTag.parentNode.replaceChild(newScriptTag, scriptTag);
-  });
+function removeScriptTags(htmlContent) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlContent, 'text/html');
+  const scripts = doc.querySelectorAll('script');
+  scripts.forEach(script => script.remove());
+  return doc.documentElement.outerHTML;
 }
 
 function observeDOMChanges() {
@@ -114,6 +104,56 @@ function observeDOMChanges() {
 
 observeDOMChanges();
 
+
+
+// async function inlineResources(newTab, fileUrl) {
+//   const baseUrl = fileUrl.substring(0, fileUrl.lastIndexOf('/'));
+
+//   setBaseUrl(newTab, baseUrl);
+
+//   try {
+//     await Promise.all([
+//       inlineStyles(newTab, baseUrl),
+//       inlineImages(newTab, baseUrl)
+//     ]);
+//   } catch (error) {
+//     console.error('Error inlining resources:', error);
+//   }
+// }
+
+// function setBaseUrl(newTab, baseUrl){
+//   const baseElement = newTab.document.createElement('base');
+//   baseElement.href = baseUrl + '/';
+//   newTab.document.head.appendChild(baseElement);
+// }
+
+// async function inlineStyles(newTab, baseUrl){
+//   const linkTags = Array.from(newTab.document.querySelectorAll('link[rel="stylesheet"]'));
+//   const fetchCssPromises = linkTags.map(tag => fetchAndReplaceStyle(tag, baseUrl));
+//   await Promise.all(fetchCssPromises);
+// }
+
+// async function inlineImages(newTab, baseUrl) {
+//   const imgTags = Array.from(newTab.document.querySelectorAll('img'));
+//   imgTags.forEach(tag => {
+//     const imgUrl = new URL(tag.src, baseUrl).href;
+//     tag.src = imgUrl;
+//   });
+// }
+
+// async function fetchAndReplaceStyle(tag, baseUrl){
+//   const cssUrl = new URL(tag.href, baseUrl).href;
+//   try {
+//     const response = await fetch(cssUrl);
+//     if (!response.ok) throw new Error(`Failed to fetch CSS: ${response.statusText}`);
+//     const cssContent = await response.text();
+//     const styleElement = document.createElement('style');
+//     styleElement.textContent = cssContent;
+//     tag.parentNode.replaceChild(styleElement, tag);
+//   } catch (error) {
+//     console.log(`Error fetching CSS from ${cssUrl}:`, error);
+//   }
+// }
 
 
 // function addButtons() {
@@ -133,13 +173,14 @@ observeDOMChanges();
 //         try {
 //           const response = await fetch(fileUrl);
 //           const htmlContent = await response.text();
+
+//           const sanitizedHtmlContent = removeScriptTags(htmlContent);
           
 //           const newTab = window.open();
 //           newTab.document.open();
-//           newTab.document.write(htmlContent);
+//           newTab.document.write(sanitizedHtmlContent);
 //           newTab.document.close();
 
-//           // Handle linked resources
 //           await inlineResources(newTab, fileUrl);
 
 //         } catch (error) {
@@ -151,52 +192,12 @@ observeDOMChanges();
 //   });
 // }
 
-// async function inlineResources(newTab, fileUrl) {
-//   const baseUrl = fileUrl.substring(0, fileUrl.lastIndexOf('/'));
-
-//   // Update base URL for relative paths
-//   const baseElement = newTab.document.createElement('base');
-//   baseElement.href = baseUrl + '/';
-//   newTab.document.head.appendChild(baseElement);
-
-//   // Inline styles
-//   const linkTags = Array.from(newTab.document.querySelectorAll('link[rel="stylesheet"]'));
-//   for (const tag of linkTags) {
-//     const cssUrl = new URL(tag.href, baseUrl).href;
-//     try {
-//       const response = await fetch(cssUrl);
-//       const cssContent = await response.text();
-//       const styleElement = newTab.document.createElement('style');
-//       styleElement.textContent = cssContent;
-//       tag.parentNode.replaceChild(styleElement, tag);
-//     } catch (error) {
-//       console.error('Error fetching CSS:', error);
-//     }
-//   }
-
-//   // Inline scripts
-//   const scriptTags = Array.from(newTab.document.querySelectorAll('script'));
-//   for (const tag of scriptTags) {
-//     if (tag.src) {
-//       const scriptUrl = new URL(tag.src, baseUrl).href;
-//       try {
-//         const response = await fetch(scriptUrl);
-//         const scriptContent = await response.text();
-//         const inlineScript = newTab.document.createElement('script');
-//         inlineScript.textContent = scriptContent;
-//         tag.parentNode.replaceChild(inlineScript, tag);
-//       } catch (error) {
-//         console.error('Error fetching script:', error);
-//       }
-//     }
-//   }
-
-//   // Fix image paths
-//   const imgTags = Array.from(newTab.document.querySelectorAll('img'));
-//   for (const tag of imgTags) {
-//     const imgUrl = new URL(tag.src, baseUrl).href;
-//     tag.src = imgUrl;
-//   }
+// function removeScriptTags(htmlContent) {
+//   const parser = new DOMParser();
+//   const doc = parser.parseFromString(htmlContent, 'text/html');
+//   const scripts = doc.querySelectorAll('script');
+//   scripts.forEach(script => script.remove());
+//   return doc.documentElement.outerHTML;
 // }
 
 // function observeDOMChanges() {
